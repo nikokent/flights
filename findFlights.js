61481575f97ac083783c6cde850c8ea1be88b6e2
nkent <nkent@eecs.wsu.edu>
2018-03-29 16:12:35 -0700

    initial commit

---
+++ findFlights.js
@@ -0,0 +1,63 @@
+var express = require('express'); 
+var bodyParser = require('body-parser');
+const request = require('request');
+
+var app = express();
+var router = express.Router();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+var testLocations = ["hnl", "nyc", "CUN", "JP", "IE", "PT", "sjd", "MX", "NL", "DK", "FR", "HR", "TH", "TR", "IT", "GR", "IS" ]
+
+var main = function(req, res){
+    var data = [];
+    var dealsCounted = 0;
+    console.log(req.body);
+
+    var flight = function(origin, destination, completion){
+        var url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/"+origin+"/"+destination+"/anytime/anytime?apikey=prtl6749387986743898559646983194";
+        request(url, { json: true }, (err, resp, body) => {
+            if (err) { return console.log(err); }
+            data.push(body);
+            completion();
+        });
+    }
+
+    var result = function(endData){
+        res.send(endData);
+    };
+
+    var compileData = function(origin, locations, completion){
+        for (index in locations){
+            flight(origin, locations[index], function(){
+                dealsCounted += 1;
+                console.log(String((dealsCounted / locations.length) * 100) + "%");
+                if (dealsCounted == locations.length){
+                    completion();
+                }
+            });
+        }
+    }
+
+    compileData(req.body.origin, req.body.locations, function(){
+        result(data);
+    });
+    
+}
+
+var http = require("http");
+setInterval(function() {
+    http.get("https://serene-stream-70888.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
+
+
+router.route('/').post(main);//POST Data with Request and Response
+
+app.use(router); //run router
+
+
+app.listen(port, '0.0.0.0', function(){
+    console.log("UP AND RUNNING!");
+});
---
+++ flights.py
@@ -0,0 +1,38 @@
+import urllib2
+import json
+
+def flight(start, end):
+    contents = urllib2.urlopen("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/"+start+"/"+end+"/anytime/anytime?apikey=prtl6749387986743898559646983194").read()
+    data = json.loads(contents)
+    deals = data["Quotes"]
+    places = data["Places"]
+    airline = data["Carriers"]
+    minPrice = deals[0]["MinPrice"]
+
+    result = 0
+    for i in range(0,len(deals)):
+        if deals[i]["MinPrice"] < minPrice:
+            result = i
+            minPrice = deals[i]["MinPrice"]
+
+    #print(deals[result])
+    #print(airline)
+    departDate = deals[result]["OutboundLeg"]["DepartureDate"]
+    returnDate = deals[result]["InboundLeg"]["DepartureDate"]
+
+    originID = deals[result]["OutboundLeg"]["OriginId"]
+    destinationID = deals[result]["InboundLeg"]["OriginId"]
+
+    for i in range(0,len(places)):
+        if originID == places[i]["PlaceId"]:
+            result = i
+    origin = places[result]["Name"]
+    for i in range(0,len(places)):
+        if destinationID == places[i]["PlaceId"]:
+            result = i
+    destination = places[result]["Name"]
+    return( "Cheapest Flight Round trip from ({0}) to ({1}) will cost {2} on leaving: {3} and returning: {4}".format(origin, destination, minPrice, departDate[:10], returnDate[:10]))
+
+destinations = ["hnl", "nyc", "CUN", "JP", "IE", "PT", "sjd", "MX", "NL", "DK", "FR", "HR", "TH", "TR", "IT", "GR", "IS" ]
+for val in destinations:
+    print(flight("sea", val))
